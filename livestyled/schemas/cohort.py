5fa172e162482c61c01651475c80c926617aa591
Yaroslav Petrov <blinder@MacBook-Pro-Yaroslav.local>
2021-06-23 18:32:10 +0300

    BE-1589: added /v4 to bulk_cohorts attachement

--- livestyled/schemas/cohort.py
+++ livestyled/schemas/cohort.py
@@ -9,8 +9,8 @@ class CohortSchema(Schema):
         model = Cohort
         api_type = 'cohorts'
         url = 'cohorts'
-        bulk_user_attach_url = 'users/{}/cohorts/bulk-attach'
-        bulk_user_detach_url = 'users/{}/cohorts/bulk-detach'
+        bulk_user_attach_url = 'v4/users/{}/cohorts/bulk-attach'
+        bulk_user_detach_url = 'v4/users/{}/cohorts/bulk-detach'
 
     title = fields.String()
     external_id = fields.String(data_key='externalId', required=False, missing=None)
