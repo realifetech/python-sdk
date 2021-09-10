3249c3645eb3685ceb8d824444be9d720fd0e0b5
Dasha Syvyuk <dashasyvyuk@Dashas-MBP.localdomain>
2021-09-10 17:22:21 +0300

    CTD-130 move to one line

--- livestyled/models/order.py
+++ livestyled/models/order.py
@@ -160,9 +160,7 @@ class Order:
 
     def diff(self, other):
         differences = {}
-        fields = (
-            'external_id', 'status'
-        )
+        fields = ('external_id', 'status')
         for field in fields:
             if getattr(self, field) != getattr(other, field):
                 differences[field] = getattr(self, field)
