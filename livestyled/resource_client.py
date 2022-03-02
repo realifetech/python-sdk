9f71419e935358b60a62e3ec0dec374f2fd5150a
Edvinas Ilcenko <edvinas@ilcenko.com>
2022-03-02 18:47:36 +0200

    Actually we don't use python <3.6 so use f-string

--- livestyled/resource_client.py
+++ livestyled/resource_client.py
@@ -601,7 +601,7 @@ class LiveStyledResourceClient(LiveStyledAPIClient):
             if value is None:
                 payload.pop(key)
         new_push_broadcast = self._api_post(
-            'v4/{}'.format(PushBroadcastSchema.Meta.publish_url),
+            f'v4/{PushBroadcastSchema.Meta.publish_url}',
             payload
         )
         return PushBroadcast(**PushBroadcastSchema().load(new_push_broadcast))
